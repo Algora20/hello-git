b136d8c2cf13cda135c866dbfff77bdf8e42562a
Alvaro <alvarotuning39@gmail.com>
2026-05-25 22:31:34 +0200

    mi primer commit

---
+++ proyecto/Program.cs
@@ -0,0 +1,50 @@
+﻿using proyecto.modelo;
+
+namespace proyecto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Usuario> listaUsuario = new List<Usuario>();
+        listaUsuario.Add(new Usuario("Jose", "1234"));
+        listaUsuario.Add(new Usuario("Maria", "1234"));
+
+        do
+        {
+            bool existe = false;
+            Console.WriteLine("Ingrese el nombre del usuario: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese contraseña del usuario: ");
+            string contrasena = Console.ReadLine();
+            foreach (Usuario u in listaUsuario)
+            {
+                if (u.getNombre().Equals(nombre))
+                {
+                    Console.WriteLine("El usuario con el nombre es " + nombre);
+                    if (u.GetContrasena() == contrasena)
+                    {
+                        Console.WriteLine("Existe usuario y la contraseña es correcta.");
+                        existe = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Existe usuario pero la contraseña es incorrecta.");
+                    }
+                    break;
+                }
+                Console.WriteLine("No existe usuario");
+                break;
+                
+            }
+
+            if (existe)
+            {
+                Usuario u = new Usuario(nombre, contrasena);
+                Console.WriteLine($"{u.getNombre()}, {u.GetContrasena()}");
+                
+                
+            }
+        } while (true);
+    }
+}
---
+++ proyecto/modelo/Usuario.cs
@@ -0,0 +1,56 @@
+﻿namespace proyecto.modelo;
+
+public class Usuario
+{
+    private string nombre;
+    private string contraseña;
+    private int edad;
+    private string informacion;
+
+    public Usuario(string nombre, string contraseña)
+    {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        edad = 0;
+    }
+
+    public int getEdad()
+    {
+        return edad;
+    }
+
+    public bool setEdad(int edad, out string mensaje)
+    {
+        mensaje = "";
+        if (!int.TryParse(edad.ToString(), out edad))
+        {
+            mensaje="edad no valida";
+            return false;
+        }
+        if (edad <= 0 || edad >= 120)
+        {
+            mensaje="edad no valida";
+            return false;
+        }
+        mensaje="edad introducida correctamente";
+        return true;
+    }
+
+    public string getNombre()
+    {
+        return nombre;
+    }
+
+    public string GetContrasena()
+    {
+        return contraseña;
+    }
+
+    public void IntroducirInformacion(string mensaje)
+    {
+        informacion = mensaje;
+    }
+    
+    
+    
+}
