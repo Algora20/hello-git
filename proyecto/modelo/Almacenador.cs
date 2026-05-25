cc0a927d13633580216d782d92d8393f6cf58ac3
Alvaro <alvarotuning39@gmail.com>
2026-05-25 22:41:02 +0200

    segundo commit y añade almacenador

---
+++ proyecto/modelo/Almacenador.cs
@@ -0,0 +1,21 @@
+﻿namespace proyecto.modelo;
+
+public class Almacenador
+{
+    private List<Usuario> usuarios;
+
+    public Almacenador()
+    {
+        usuarios = new List<Usuario>();
+    }
+
+    private bool ExisteUsuario(string nombre)
+    {
+        bool resultado = usuarios.Any(u=>u.getNombre()==nombre);
+        return resultado;
+    }
+    public void AgregarUsuario(Usuario usuario)
+    {
+        
+    }
+}
